adc160e30106ea3c9c1247dbd8c368234ade2949
mago_d_Mercurio <davila.feria@gmail.com>
2023-07-29 11:39:09 -0500

    update name

--- src/pages/index.js
+++ src/pages/index.js
@@ -100,12 +100,12 @@ export default function Home() {
                   ))}
                 </div>
                 <div className="py-6">
-                  <Link
+                  <a
                     href="/login"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
                     Log in
-                  </Link>
+                  </a>
                 </div>
               </div>
             </div>
