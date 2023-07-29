25faf82255827b172dd94b1501525e1d5fc1fb72
mago_d_Mercurio <davila.feria@gmail.com>
2023-07-29 06:30:59 -0600

    create products.js

---
+++ src/services/api/products.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import endPoints from "@services/api";
+
+const addProduct = async (body) => {
+  const config = {
+    Headers: {
+      accept: "*/*",
+      "content-Type": "application/json",
+    },
+  };
+  const response = await axios.post(
+    endPoints.products.addProduct,
+    body,
+    config,
+  );
+  return response.data;
+};
+
+export { addProduct };
